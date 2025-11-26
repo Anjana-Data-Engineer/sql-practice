e08f0ed1600ec33277e510159df316ccedb627e6
Anjana-Data-Engineer <anjanaseetharamanworkmail@gmail.com>
2025-11-26 20:32:22 +0100

    Create select.sql

---
+++ basic-queries/select.sql
@@ -0,0 +1,10 @@
+-- Problem:
+-- Retrieve all customer names and their cities from the Customers table.
+
+SELECT
+    customer_name,
+    city
+FROM customers;
+
+-- Explanation:
+-- A simple SELECT statement that fetches specific columns.
