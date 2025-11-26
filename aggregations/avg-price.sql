98448af20c104e4a978fd536c455bf58a107cc2e
Anjana-Data-Engineer <anjanaseetharamanworkmail@gmail.com>
2025-11-26 20:46:12 +0100

    Create avg-price.sql

---
+++ aggregations/avg-price.sql
@@ -0,0 +1,9 @@
+-- Problem:
+-- Calculate the average price of all products.
+
+SELECT
+    AVG(price) AS average_price
+FROM products;
+
+-- Explanation:
+-- AVG returns the mean value of a numeric column.
