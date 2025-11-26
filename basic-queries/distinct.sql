54325a4557fd6aef9807c69341cbaadbede61abf
Anjana-Data-Engineer <anjanaseetharamanworkmail@gmail.com>
2025-11-26 20:43:57 +0100

    Create distinct.sql

---
+++ basic-queries/distinct.sql
@@ -0,0 +1,9 @@
+-- Problem:
+-- List all unique cities where customers live.
+
+SELECT DISTINCT
+    city
+FROM customers;
+
+-- Explanation:
+-- DISTINCT removes duplicate values from the result set.
