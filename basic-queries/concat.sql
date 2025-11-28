2457fa69fe7e51cfd871839162d780c461c9e540
Anjana-Data-Engineer <anjanaseetharamanworkmail@gmail.com>
2025-11-28 19:04:47 +0100

    Create concat.sql

---
+++ basic-queries/concat.sql
@@ -0,0 +1,13 @@
+--The CONCAT() function adds two or more strings together.
+--Syntax:
+--CONCAT(string1, string2, ...., string_n)
+
+SELECT CONCAT('SQL', ' is', ' fun!');
+
+--Concat With +
+--The + operator allows you to add two or more strings together.
+
+--Syntax:
+--string1 + string2 + string_n
+
+SELECT 'SQL' + ' is' + ' fun!';
