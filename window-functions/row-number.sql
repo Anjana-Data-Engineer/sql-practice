3ef9cabc7d401b5bdbeef217c0280b1090af078c
Anjana-Data-Engineer <anjanaseetharamanworkmail@gmail.com>
2025-12-03 10:32:16 +0100

    Update row-number.sql

--- window-functions/row-number.sql
+++ window-functions/row-number.sql
@@ -1,4 +1,7 @@
 -- Problem:
+-- ROW_NUMBER() is a window function that assigns a sequential number to rows within each partition, 
+-- in the order defined by the ORDER BY inside the OVER(...) clause. The PARTITION BY is optional—if you omit it, the entire result set is treated as a single partition.
+--It creates a window and assigns row number to them
 -- Assign a row number to each order for every customer,
 -- ordered by order date.
 
